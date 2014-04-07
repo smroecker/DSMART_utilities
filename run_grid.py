641cd20d86c362a934fd306066cadaa866cfc085
Nathaniel Chaney <chaneyna@gmail.com>
2014-04-07 00:29:53 -0400

    Finished the mapping to different cells

--- run_grid.py
+++ run_grid.py
@@ -11,6 +11,18 @@ def Run_DSMART(region_dir):
  
  return
 
+def Region_Cutout(region_dir,id):
+
+ vars = ['maxcl1_class',]
+ for var in vars:
+  file_in = '%s/output/probabilities/%s.img' % (region_dir,var)
+  if os.path.exists('%s/%s' % (output_dir,var)) == False:
+   os.mkdir('%s/%s' % (output_dir,var))
+  file_out = '%s/%s/%s_%d.tif' % (output_dir,var,var,id)
+  os.system("gdalwarp -te %.16f %.16f %.16f %.16f -overwrite %s %s" % (dims['minlon_c'],dims['minlat_c'],dims['maxlon_c'],dims['maxlat_c'],file_in,file_out))
+
+ return
+
 #Iterate through the region with overlapping grids
 res = 0.0002777777777780
 x_overlap = 600
@@ -20,7 +32,15 @@ ny = 900
 minlat = 38.75
 minlon = -89.0
 n = 3
+
+#Prepare directories
 dir = '/home/ice/nchaney/PROJECTS/SYDNEY2014/HOMOGENIZATION/dsmart_example/illinois'
+output_dir = '%s/output' % dir
+if os.path.exists(output_dir) == False:
+ os.mkdir(output_dir)
+os.system('rm -rf %s/*' % output_dir)
+
+#Run algorithm on chunks
 for i in xrange(n):
  for j in xrange(n):
   print i,j
@@ -34,6 +54,10 @@ for i in xrange(n):
   dims['maxlat'] = minlat + ymax*res
   dims['minlon'] = minlon + xmin*res
   dims['maxlon'] = minlon + xmax*res
+  dims['minlat_c'] = minlat + (ymin+300)*res
+  dims['minlon_c'] = minlon + (xmin+300)*res
+  dims['maxlat_c'] = minlat + (ymin+600)*res
+  dims['maxlon_c'] = minlon + (xmin+600)*res
   dims['res'] = res
   dims['nx'] = nx
   dims['ny'] = ny
@@ -41,9 +65,15 @@ for i in xrange(n):
 
   #Prepare the data
   region_dir = '%s/region_%d' % (dir,dims['id'])
-  dsmart_utilities.prepare_input(dims,region_dir)
+  #dsmart_utilities.prepare_input(dims,region_dir)
 
   #Run dsmart (Change to directory first)
-  #os.chdir(region_dir)
-  #os.system('python configuration.py')
-  Run_DSMART(region_dir)
+  #Run_DSMART(region_dir)
+
+  #Cut out the region of interst
+  Region_Cutout(region_dir,dims['id'])
+
+#Merge each variables files
+vars = ['maxcl1_class',]
+for var in vars:
+ os.system('gdalwarp %s/%s/*.tif %s/%s.tif' % (output_dir,var,output_dir,var))
